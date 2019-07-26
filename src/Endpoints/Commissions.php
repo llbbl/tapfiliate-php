83a9ec0e41cca1efb574510679ae2c7286119f6d
LegacyBetaDev <developers@legacybeta.com>
2019-07-26 20:15:00 +0530

    Add additional Commissions methods

--- src/Endpoints/Commissions.php
+++ src/Endpoints/Commissions.php
@@ -14,9 +14,45 @@ class Commissions implements EndpointInterface
         $this->adapter = $adapter;
     }
 
-    public function listCommissions()
+    public function listCommissions(): array
     {
         $response = $this->adapter->get($this->url);
+
+        return json_decode($response->getBody());
+    }
+
+    public function getCommission(int $commissionId): object
+    {
+        $response =  $this->adapter->get($this->url . '/' . $commissionId . '/');
+
+        return json_decode($response->getBody());
+    }
+
+    public function updateCommission(int $commissionId, float $amount, string $comment = null): object
+    {
+        $query = [
+            'amount' => $amount
+        ];
+
+        if ($comment !== null) {
+            $query['comment'] = $comment;
+        }
+        
+        $response =  $this->adapter->patch($this->url . '/' . $commissionId . '/', $query);
+
+        return json_decode($response->getBody());
+    }
+
+    public function approveCommission(int $commissionId): object
+    {
+        $response =  $this->adapter->put($this->url . '/' . $commissionId . '/approved/');
+
+        return json_decode($response->getBody());
+    }
+
+    public function disapproveCommission(int $commissionId): object
+    {
+        $response =  $this->adapter->delete($this->url . '/' . $commissionId . '/approved/');
 
         return json_decode($response->getBody());
     }
